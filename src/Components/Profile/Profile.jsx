8c5baa3e83041249b7253fb3fd1b0ac8d797b596
Joaquin <jaltable@fi.uba.ar>
2023-09-13 23:04:24 -0300

    first commit

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import styles from './Profile.module.css';
+
+export default function Profile(){
+    return(
+        <div className={styles.profile}>
+            <h2>Front End Developer</h2>
+            <h3>Joaquin Altable</h3>
+            <p>joaquin el frontend Developer</p>
+        </div>
+    );
+};
