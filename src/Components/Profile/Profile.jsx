2e6eeee8c4de60a3ee9060532fc3e62c630ddda2
Joaquin <jaltable@fi.uba.ar>
2023-09-14 14:03:29 -0300

    added images to the tech stack section

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -3,13 +3,26 @@ import styles from './Profile.module.css';
 
 export default function Profile(){
     return(
-        <div className={styles.profile}>
-            <div className={styles.profileHeader}>
-                <h2>Front End Developer</h2>
-                <h3>Joaquin Altable</h3>
-                <p>joaquin el frontend Developer</p>
+        <section className={styles.profile}>
+            <div className={styles.profileWrapper}>
+                <div className={styles.profileHeader}>
+                    <h2>Front End Developer</h2>
+                    <h3>Joaquin Altable</h3>
+                    <p>joaquin el frontend Developer</p>
+                </div>
+
+                <div className={styles.imageWrapper}>
+                    <img src="../../img/cheeto-chan.PNG" alt="cheeto chan" />
+                </div>
             </div>
-            <img src="../../img/cheeto-chan.PNG" alt="cheeto chan" />
-        </div>
+
+            <div className={styles.techStack}>
+                <h3>Tech stack:</h3>
+
+                <img src="../../img/smiley.png" alt="logo1" />
+                <img src="../../img/divine.png" alt="logo2" />
+                <img src="../../img/osaka1.jpg" alt="logo3" />
+            </div>
+        </section>
     );
 };
