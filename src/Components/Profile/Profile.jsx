d44e85cc0029dd3c0e20cd8d0438d6c0e928a252
Joaquin <jaltable@fi.uba.ar>
2023-09-13 23:30:39 -0300

    added tech stack to 'Profile'

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,11 +1,15 @@
 import styles from './Profile.module.css';
 
+
 export default function Profile(){
     return(
         <div className={styles.profile}>
-            <h2>Front End Developer</h2>
-            <h3>Joaquin Altable</h3>
-            <p>joaquin el frontend Developer</p>
+            <div className={styles.profileHeader}>
+                <h2>Front End Developer</h2>
+                <h3>Joaquin Altable</h3>
+                <p>joaquin el frontend Developer</p>
+            </div>
+            <img src="../../img/cheeto-chan.PNG" alt="cheeto chan" />
         </div>
     );
 };
