04a1d05a3a210190c35b9f2050f5bcc2940fc384
Joaquin <jaltable@fi.uba.ar>
2023-09-15 15:20:25 -0300

    applied flexbox to 'ProjectItem'

--- src/Components/ProjectItem/ProjectItem.jsx
+++ src/Components/ProjectItem/ProjectItem.jsx
@@ -5,22 +5,28 @@ export default function ProjectItem({project}){
 
     return(
         <div className={styles.item}>
-            <img src={imgUrl} alt="Project" />
+            <div className={styles.itemImage}>
+                <img src={imgUrl} alt="Project" />
+            </div>
 
-            <h3>{name}</h3>
+            <div className={styles.itemText}>
+                <h3>{name}</h3>
 
-            <p>{description}</p>
+                <p>{description}</p>
 
-            {techStack.map((tech) => <h4 key={tech}><strong>{tech}</strong></h4>)}
+                <div className={styles.techStack}>
+                    {techStack.map((tech) => <h4 key={tech}><strong>{tech}</strong></h4>)}
+                </div>
 
-            <div className={styles.externalLinks}>
-                <a href={gitUrl}>
-                    GitHub
-                </a>
+                <div className={styles.externalLinks}>
+                    <a href={gitUrl}>
+                        GitHub
+                    </a>
 
-                <a href={demoUrl}>
-                    Live Demo
-                </a>
+                    <a href={demoUrl}>
+                        Live Demo
+                    </a>
+                </div>
             </div>
         </div>
     )
