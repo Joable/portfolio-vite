ca9c7ead7ee59a4ddb822876dddb8049338cdd74
Joaquin <jaltable@fi.uba.ar>
2023-09-15 16:59:28 -0300

    made the html template of 'Contact'

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,7 +3,15 @@ import styles from './Contact.module.css';
 export default function Contact(){
     return(
         <div className={styles.contact}>    
-        contact
+        <h3>Contact</h3>
+        <h2>Contact me!</h2>
+
+        <div className={styles.email}>
+            <img src="../../img/smiley2.png" alt="smile" />
+
+            <h3>Email</h3>
+            <p>Joaodasilva@shroogle.com</p>
+        </div>
 
         </div>
     );
