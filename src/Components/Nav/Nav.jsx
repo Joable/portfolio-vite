d665d2f5bf37fb303de7db83db0852da1cb71e4d
Joaquin <jaltable@fi.uba.ar>
2023-09-16 14:18:00 -0300

    added content to 'Nav'

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -4,6 +4,16 @@ export default function Nav(){
     return(
         <nav>
             <a href="">Logo</a>
+
+            <div className={styles.pageNavigation}>
+                 <a href="#">Home</a>
+                 
+                 <a href="#">About</a>
+                 
+                 <a href="#">Projects</a>
+                 
+                 <a href="#">Contact</a>
+            </div>
         </nav>
     );
 }
