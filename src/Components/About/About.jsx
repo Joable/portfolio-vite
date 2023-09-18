5fbaeacca9fe8f604ddcd260bcba397f74179a59
Joaquin <jaltable@fi.uba.ar>
2023-09-18 18:07:30 -0300

    now 'About' takes 100 view height and its centered

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,23 +3,29 @@ import styles from './About.module.css';
 export default function About(){
     return(
         <section className={styles.about}>
-            <div className={styles.aboutText}>
-                <h3>ABOUT ME</h3>
-                <h2>THE BIGGEST OF THEM ALL</h2>
 
-                <p>
-                    Curabitur congue aliquet pellentesque. Aenean at finibus lacus. Aliquam molestie, 
-                    odio sed ornare ultrices, magna augue faucibus urna, quis malesuada elit mauris ac massa. 
-                    Nulla fermentum turpis a purus pharetra, ut elementum ex euismod. Mauris malesuada nunc dolor, 
-                    id faucibus neque laoreet quis. Pellentesque odio est, sollicitudin eu ante ac, consectetur rhoncus augue. 
-                    Nam sagittis efficitur nisl et molestie. Etiam porta nisl sed turpis cursus consequat. 
-                    Suspendisse sagittis tellus tempus, hendrerit ipsum quis, sagittis magna. Cras id arcu ipsum.
-                </p>
-            </div>
+            <div className={styles.aboutWrapper}>
 
-            <div className={styles.aboutImage}>
-                <img src="../../img/whenithits.jpg" alt="About" />
+                <div className={styles.aboutText}>
+                    <h3>ABOUT ME</h3>
+                    <h2>THE BIGGEST OF THEM ALL</h2>
+
+                    <p>
+                        Curabitur congue aliquet pellentesque. Aenean at finibus lacus. Aliquam molestie, 
+                        odio sed ornare ultrices, magna augue faucibus urna, quis malesuada elit mauris ac massa. 
+                        Nulla fermentum turpis a purus pharetra, ut elementum ex euismod. Mauris malesuada nunc dolor, 
+                        id faucibus neque laoreet quis. Pellentesque odio est, sollicitudin eu ante ac, consectetur rhoncus augue. 
+                        Nam sagittis efficitur nisl et molestie. Etiam porta nisl sed turpis cursus consequat. 
+                        Suspendisse sagittis tellus tempus, hendrerit ipsum quis, sagittis magna. Cras id arcu ipsum.
+                    </p>
+                </div>
+
+                <div className={styles.aboutImage}>
+                    <img src="../../img/whenithits.jpg" alt="About" />
+                </div>
+
             </div>
+            
         </section>
     );
 }
