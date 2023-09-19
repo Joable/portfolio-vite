5e0d61d1ed6da97174678cd535b75b4e0297aa68
Joaquin <jaltable@fi.uba.ar>
2023-09-19 18:52:18 -0300

    centered 'Contact'

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,16 +2,18 @@ import styles from './Contact.module.css';
 
 export default function Contact(){
     return(
-        <div className={styles.contact}>    
-            <h3>Contact</h3>
-            <h2>Contact me!</h2>
+        <section className={styles.contact}>
+            <div className={styles.contactWrapper}>    
+                <h3>Contact</h3>
+                <h2>Contact me!</h2>
 
-            <div className={styles.email}>
-                <img src="../../img/smiley2.png" alt="smile" />
+                <div className={styles.email}>
+                    <img src="../../img/smiley2.png" alt="smile" />
 
-                <h3>Email</h3>
-                <p>Joaodasilva@shroogle.com</p>
+                    <h3>Email</h3>
+                    <p>Joaodasilva@shroogle.com</p>
+                </div>
             </div>
-        </div>
+        </section>
     );
 }
