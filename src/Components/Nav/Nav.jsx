c86d6739d3a42631c8cb95af63c493b5bfb4df72
Joaquin <jaltable@fi.uba.ar>
2023-09-20 17:54:29 -0300

    now 'Nav' get each html element for each button

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -6,26 +6,31 @@ import {
 } from 'react';
 
 export default function Nav(){
-    const [projectsId, setProjectsId] = useState("");
+    const [profileElement, setProfileElement] = useState("");
+    const [aboutElement, setAboutElement] = useState("");
+    const [projectsElement, setProjectsElement] = useState("");
+    const [contactElement, setContactElement] = useState("");
+    
 
     useEffect(() => {
-        setProjectsId(document.getElementById('projects'));
-
-        console.log(projectsId)
+        setProfileElement(document.getElementById('profile'));
+        setAboutElement(document.getElementById('about'));
+        setProjectsElement(document.getElementById('projects'));
+        setContactElement(document.getElementById('contact'));
     },[]);
 
     return(
         <nav>
-            <a href="">Logo</a>
+            <a href="#">Logo</a>
 
             <div className={styles.pageNavigation}>
-                 <button onClick={() => console.log(1)}>Home</button>
+                 <button onClick={() => console.log(profileElement)}>Profile</button>
                  
-                 <button onClick={() => console.log(1)}>About</button>
+                 <button onClick={() => console.log(aboutElement)}>About</button>
 
-                 <button onClick={() => console.log(1)}>Projects</button>
+                 <button onClick={() => console.log(projectsElement)}>Projects</button>
                  
-                 <button onClick={() => console.log(1)}>Contact</button>
+                 <button onClick={() => console.log(contactElement)}>Contact</button>
             </div>
         </nav>
     );
